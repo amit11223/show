711b3d841757475676711ef1b4991f59e06adc09
amit11223 <120323415+amit11223@users.noreply.github.com>
2023-02-27 15:17:31 +0000

    working menu items

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -11,7 +11,7 @@ import {
 } from '@ionic/react';
 
 import { useLocation } from 'react-router-dom';
-import { archiveOutline, archiveSharp, bookmarkOutline, heartOutline, heartSharp, mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, trashOutline, trashSharp, warningOutline, warningSharp } from 'ionicons/icons';
+import { archiveOutline, archiveSharp, bookmarkOutline, heartOutline, heartSharp, mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, trashOutline, trashSharp, warningOutline, warningSharp,alarmOutline,cardOutline,videocamSharp,accessibilityOutline } from 'ionicons/icons';
 import './Menu.css';
 
 interface AppPage {
@@ -22,45 +22,35 @@ interface AppPage {
 }
 
 const appPages: AppPage[] = [
+  
   {
-    title: 'Inbox',
-    url: '/page/Inbox',
-    iosIcon: mailOutline,
-    mdIcon: mailSharp
+    title: 'רכישת רטיסים',
+    url: '/page/purchase of tickets',
+    iosIcon: cardOutline,
+    mdIcon: cardOutline
+  },
+  
+  {
+    title: 'לוח הצגות',
+    url: '/page/play schedule',
+    iosIcon: videocamSharp,
+    mdIcon: videocamSharp
   },
   {
-    title: 'Outbox',
-    url: '/page/Outbox',
-    iosIcon: paperPlaneOutline,
-    mdIcon: paperPlaneSharp
+    title: 'שחקנים',
+    url: '/page/actors',
+    iosIcon: accessibilityOutline,
+    mdIcon: accessibilityOutline
   },
   {
-    title: 'Favorites',
-    url: '/page/Favorites',
-    iosIcon: heartOutline,
-    mdIcon: heartSharp
-  },
-  {
-    title: 'Archived',
-    url: '/page/Archived',
-    iosIcon: archiveOutline,
-    mdIcon: archiveSharp
-  },
-  {
-    title: 'Trash',
-    url: '/page/Trash',
-    iosIcon: trashOutline,
-    mdIcon: trashSharp
-  },
-  {
-    title: 'Spam',
-    url: '/page/Spam',
+    title: 'התנתקות',
+    url: '/page/log out',
     iosIcon: warningOutline,
-    mdIcon: warningSharp
+    mdIcon: warningOutline
   }
 ];
 
-const labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+
 
 const Menu: React.FC = () => {
   const location = useLocation();
@@ -83,15 +73,7 @@ const Menu: React.FC = () => {
           })}
         </IonList>
 
-        <IonList id="labels-list">
-          <IonListHeader>Labels</IonListHeader>
-          {labels.map((label, index) => (
-            <IonItem lines="none" key={index}>
-              <IonIcon aria-hidden="true" slot="start" icon={bookmarkOutline} />
-              <IonLabel>{label}</IonLabel>
-            </IonItem>
-          ))}
-        </IonList>
+       
       </IonContent>
     </IonMenu>
   );
